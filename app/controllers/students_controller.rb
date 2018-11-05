6fc8fed173b5f0550c7e1d954aa628a027c66882
Patricia Nykaza <pnykaza@calstatela.edu>
2018-11-05 19:14:29 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,7 +12,7 @@ class StudentsController < ApplicationController
 	end
 
 	def create
-	  @student = Student.new(student_params(:title, :description))
+	  @student = Student.new(student_params(:first_name, :last_name))
 	  @student.save
 	  redirect_to student_path(@student)
 	end
@@ -30,6 +30,6 @@ class StudentsController < ApplicationController
 	private
 
 	def student_params(*args)
-		params.require(:post).permit(*args)
+		params.require(:student).permit(*args)
 	end
 end
