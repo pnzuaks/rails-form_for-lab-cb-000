d7db0e476b8d9a73eb3331469e7c171e9f002c29
Patricia Nykaza <pnykaza@calstatela.edu>
2018-11-05 19:11:55 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,30 +1,30 @@
-class PostsController < ApplicationController
+class StudentsController < ApplicationController
 	def index
-		@posts = Post.all
+		@students = Student.all
 	end
 
 	def show
-		@post = Post.find(params[:id])
+		@post = Student.find(params[:id])
 	end
 
 	def new
-		@post = Post.new
+		@post = Student.new
 	end
 
 	def create
-	  @post = Post.new(post_params(:title, :description))
+	  @post = Student.new(post_params(:title, :description))
 	  @post.save
 	  redirect_to post_path(@post)
 	end
 
 	def update
-	  @post = Post.find(params[:id])
+	  @post = Student.find(params[:id])
 	  @post.update(post_params(:title))
 	  redirect_to post_path(@post)
 	end
 
 	def edit
-	  @post = Post.find(params[:id])
+	  @post = Student.find(params[:id])
 	end
 
 	private
