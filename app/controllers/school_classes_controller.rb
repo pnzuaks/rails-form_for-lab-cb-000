ccbb993fe3f008127159df1a75a891f3ad965069
Patricia Nykaza <pnykaza@calstatela.edu>
2018-11-05 19:19:15 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -25,7 +25,7 @@ class SchoolClassesController < ApplicationController
 
 	private
 
-	def student_params(*args)
-		params.require(:student).permit(*args)
+	def school_class_params(*args)
+		params.require(:school_class).permit(*args)
 	end
 end
